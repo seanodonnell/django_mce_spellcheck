8c54f4b343db4034c6b6dfc5ed57ff57d901cf2c
Sean O'Donnell <sean@odonnell.nu>
2016-11-22 00:00:49 +0000

    updating version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 from setuptools import setup, find_packages
 
-version = '0.1.1'
+version = '0.1.2'
 
 setup(name='django_mce_spellcheck',
     description='Django app that provides a backend for the tinymce spellcheck plugin. Particularly useful for Mezzanine',
